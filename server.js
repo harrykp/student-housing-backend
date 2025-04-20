eb2939f6a3443fc6594727fea18dd2c3e30fadae
harrykp <harrykp@users.noreply.github.com>
2025-04-20 21:39:47 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const pool = require('./db/db'); // Correct path to db.js
 
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 // Middleware
 app.use(cors());
 app.use(express.json());
