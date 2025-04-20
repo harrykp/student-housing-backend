201f01acfc09140bd2f5f2ab48723c42f88b31c9
harrykp <harrykp@users.noreply.github.com>
2025-04-20 17:41:20 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config(); // Load environment variables
-const pool = require('./db'); // Database connection
+const pool = require('./db/db'); // Correct path to db.js
 
 const app = express();
 
