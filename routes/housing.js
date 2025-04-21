c2e4443336c85d74f55532dd2aa7121243b2b821
harrykp <harrykp@users.noreply.github.com>
2025-04-21 20:07:31 +0000

    Update housing.js

--- routes/housing.js
+++ routes/housing.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db/db');
+const { getHousingOptions } = require('../controllers/housingController');
 
-// Get available housing options
-router.get('/', async (req, res) => {
-    try {
-        const housingOptions = await db.query('SELECT * FROM hostels');
-        res.status(200).json(housingOptions.rows);
-    } catch (error) {
-        console.error('Error fetching housing options:', error.message);
-        res.status(500).json({ message: 'Server error.' });
-    }
-});
+// Route for fetching housing options
+router.get('/', getHousingOptions);
 
 module.exports = router;
