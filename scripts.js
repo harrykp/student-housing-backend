8b85807e8f9426f57abe97d98a06902fe2a5b56d
harrykp <harrykp@users.noreply.github.com>
2025-04-24 23:33:04 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const db = require('./db');
 
-// Example script: Fetch all users
+// Example script: Fetch all students
 (async () => {
-  console.log('Fetching all users...');
-  const result = await db.query('SELECT * FROM users');
+  console.log('Fetching all students...');
+  const result = await db.query('SELECT * FROM users WHERE role = $1', ['student']);
   console.log(result.rows);
 })();
