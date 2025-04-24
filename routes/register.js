6dd36203a071d8476748ab780a13cafdf346a8cd
harrykp <harrykp@users.noreply.github.com>
2025-04-24 13:30:55 +0000

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { registerStudent } = require('../controllers/registerController');
+const { registerUser } = require('../controllers/registerController');
 
 // Route for registering a new student
-router.post('/register', registerStudent);
+router.post('/register', registerUser);
 
 module.exports = router;
