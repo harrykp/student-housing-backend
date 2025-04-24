d3b3840165a7a5161a7c9329ebbe231ec7e1195b
harrykp <harrykp@users.noreply.github.com>
2025-04-24 18:20:12 +0000

    Update activityLogs.js

--- routes/activityLogs.js
+++ routes/activityLogs.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const { getActivityLogs, createActivityLog } = require('../controllers/activityLogsController');
 const router = express.Router();
-const { getActivityLogs } = require('../controllers/activityLogsController');
 
-// Route for fetching activity logs
 router.get('/', getActivityLogs);
+router.post('/', createActivityLog);
 
 module.exports = router;
