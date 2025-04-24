befcb11b102ab0b71bdda25449b0c71cf3a45b8d
harrykp <harrykp@users.noreply.github.com>
2025-04-24 13:17:56 +0000

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -10,14 +10,14 @@ const registerStudent = async (req, res) => {
 
     try {
         // Check if student already exists
-        const studentExists = await db.query('SELECT * FROM students WHERE email = $1', [email]);
+        const studentExists = await db.query('SELECT * FROM users WHERE email = $1', [email]);
         if (studentExists.rows.length > 0) {
             return res.status(400).json({ message: 'User already exists.' });
         }
 
         // Insert new student
         await db.query(
-            'INSERT INTO users (name, email, phone,password) VALUES ($1, $2, $3, $4)',
+            'INSERT INTO users (name, email, phone, password) VALUES ($1, $2, $3, $4)',
             [name, email, phone, password]
         );
         res.status(201).json({ message: 'User registered successfully.' });
