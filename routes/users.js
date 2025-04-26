8d68fd260aae2d4f0cd68842cdb7859b37bc7d0c
harrykp <harrykp@users.noreply.github.com>
2025-04-26 07:11:49 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,8 @@
-// routes/users.js
 const express = require('express');
 const { getUsers, createUser } = require('../controllers/usersController');
 const router = express.Router();
 
-// GET /users → fetch all users
-router.get('/', getUsers);
-
-// POST /users → register a new user
-router.post('/', createUser);
+router.get('/',    getUsers);
+router.post('/',   createUser);
 
 module.exports = router;
