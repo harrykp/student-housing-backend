38be39719e7d3457ab234c0d53641cc2a27ee200
harrykp <harrykp@users.noreply.github.com>
2025-04-26 21:37:32 +0000

    sdfs

--- routes/housing.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getHousingOptions } = require('../controllers/housingController');
-
-// Route for fetching housing options
-router.get('/', getHousingOptions);
-
-module.exports = router;
